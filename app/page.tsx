031aeb853fab5dcefcc27d32e2eb88b941771969
Mateusz Bykowicz <mbykowicz@gmail.com>
2025-08-07 11:29:37 +0100

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from './components/sections/hero'
 import Navigation from './components/sections/navigation'
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-neutral-200'>
       <Navigation />
+      <Hero />
     </div>
   )
 }
